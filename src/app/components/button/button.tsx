8b23dfe1aa28afaf5215bf1619e3c4efdc98d1fe
Caio Silva <contato@caiosb.com>
2025-03-03 23:20:47 -0300

    feat: add biomejs (#3)

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -1,13 +1,17 @@
-import { ButtonProps } from "@/app/components/button/types";
+import type { ButtonProps } from "@/app/components/button/types";
 import { tv } from "tailwind-variants";
 
 const button = tv({
-    base: "h-20 border-4 px-8 py-2.5 rounded-lg transition-all shadow-3xl hover:shadow-5xl hover:-translate-1 cursor-pointer",
-    variants: {
-        color: {yellow:"bg-yellow-400"},
-    },
-})
+	base: "h-20 border-4 px-8 py-2.5 rounded-lg transition-all shadow-3xl hover:shadow-5xl hover:-translate-1 cursor-pointer",
+	variants: {
+		color: { yellow: "bg-yellow-400" },
+	},
+});
 
-export default function Button(props : ButtonProps) {
-    return <button className={button({color: "yellow"})}>{props.children}</button>
+export default function Button(props: ButtonProps) {
+	return (
+		<button type="button" className={button({ color: "yellow" })}>
+			{props.children}
+		</button>
+	);
 }
--- src/app/components/button/types.ts
+++ src/app/components/button/types.ts
@@ -1,3 +1,3 @@
-import {HTMLAttributes} from "react";
+import type { HTMLAttributes } from "react";
 
-export type ButtonProps = HTMLAttributes<HTMLButtonElement> & {}
+export type ButtonProps = HTMLAttributes<HTMLButtonElement> & {};
