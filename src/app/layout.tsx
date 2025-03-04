1ebfdccca0131962c98b3056f9325c7a5fabc3c5
Caio Silva <contato@caiosb.com>
2025-03-03 22:21:56 -0300

    feat: add cabin, unigeo and nunito font family (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
+import localFont from "next/font/local"
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const nunito = Nunito({
+  variable: "--font-nunito",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const unigeo64 = localFont({
+  src: [{
+    path: "./assets/fonts/unigeo64-bold.ttf",
+  }],
+  variable: "--font-unigeo64",
+  weight: "bold",
+  display: "swap",
+  preload: true,
 });
 
 export const metadata: Metadata = {
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nunito.variable} ${unigeo64.variable} antialiased`}
       >
         {children}
       </body>
