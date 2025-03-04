b5c64eded7a1515903c464b201c980df46ecafe2
Caio Silva <contato@caiosb.com>
2025-03-04 10:20:54 -0300

    styles: button styles change

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -2,7 +2,7 @@ import type { ButtonProps } from "@/app/components/button/types";
 import { tv } from "tailwind-variants";
 
 const button = tv({
-	base: "h-20 border-4 px-8 py-2.5 rounded-lg transition-all shadow-3xl hover:shadow-5xl hover:-translate-1 cursor-pointer",
+	base: "h-14 border-4 px-4 py-2.5 rounded-lg transition-all shadow-3xl hover:shadow-5xl hover:-translate-1 cursor-pointer",
 	variants: {
 		color: { yellow: "bg-yellow-400" },
 	},
