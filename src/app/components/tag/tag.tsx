7b34d64fd887d5facf27ebc0755428075ca28be3
Caio Silva <contato@caiosb.com>
2025-03-06 19:40:01 -0300

    refactor: tag component styles change and children render method

--- src/app/components/tag/tag.tsx
+++ src/app/components/tag/tag.tsx
@@ -2,7 +2,7 @@ import { tv } from "tailwind-variants";
 import type { TagProps } from "./types";
 
 const tag = tv({
-	base: "w-fit font-nunito font-black px-8 py-1 rounded-full border-2 transition-all hover:-translate-1 shadow-3xl hover:shadow-5xl",
+	base: "w-fit font-nunito font-black px-8 py-1 rounded-full border-2 transition-all hover:-translate-1 active:-translate-1 shadow-3xl hover:shadow-5xl active:shadow-5xl cursor-default",
 	variants: {
 		color: {
 			fuchsia: "bg-fuchsia-300",
@@ -15,5 +15,9 @@ const tag = tv({
 });
 
 export default function Tag(props: TagProps) {
-	return <div className={tag({ color: props.color })}>{props.label}</div>;
+	return (
+		<div className={tag({ color: props.color })}>
+			{props?.children || props.label}
+		</div>
+	);
 }
